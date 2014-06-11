b0bd052a98d4669d254c481bcf243565d045afb5
Colin Jones <colinsjones11@gmail.com>
2014-06-11 14:25:38 -0500

    some Restforce working! Need to add user_id

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,14 +3,21 @@ class AuthenticationsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     current_user.authentications.find_or_create_by(:provider => auth['provider'], :uid => auth['uid'])
-    flash[:notice] = "Authentication Successful"
+
+    current_user.provider = auth.provider
+    current_user.uid = auth.uid
+    current_user.oauth_token = auth.credentials.token
+    current_user.refresh_token = auth.credentials.refresh_token
+    current_user.instance_url = auth.credentials.instance_url
+    # current_user.sfdc_user_id = auth.extra.user_id
+    current_user.save
+
     redirect_to "/users/#{current_user.id}"
   end
 
   def destroy
     @authentication = current_user.authentications.find(params[:id])
     @authentication.destroy
-    flash[:notice] = "Authentication destroyed"
     redirect_to "/users/#{current_user.id}"
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @team = @user.team
     @league = @team.league
-    @authentications = current_user.authentications.all
+
+    @client = Restforce.new :oauth_token => current_user.oauth_token,
+      :refresh_token => current_user.refresh_token,
+      :instance_url  => current_user.instance_url,
+      :client_id     => SALESFORCE_CONFIG['key'],
+      :client_secret => SALESFORCE_CONFIG['secret']
+
+    @accounts = @client.query("SELECT Name FROM Account WHERE OwnerId = '005i0000002QZPlAAO'")
   end
 
 end
